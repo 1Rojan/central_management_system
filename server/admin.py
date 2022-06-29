c87ab1f059ac37f14263f93d6dcb422d5dd559b1
1Rojan <rznsnr11@gmail.com>
2022-06-29 14:27:24 +0545

    refactored model name

--- server/admin.py
+++ server/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(ServerType)
-admin.site.register(Diskype)
+admin.site.register(DiskType)
 admin.site.register(Owner)
 admin.site.register(Location)
+admin.site.register(Server)
