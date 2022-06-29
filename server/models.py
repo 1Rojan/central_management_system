eb81242c56a28b1d207c3f15ad29209803adaf4d
1Rojan <rznsnr11@gmail.com>
2022-06-29 14:28:04 +0545

    server model

--- server/models.py
+++ server/models.py
@@ -5,10 +5,13 @@ from django.db import models
 class CommonInfo(models.Model):
   name = models.CharField(max_length=63)
 
+  def __str__(self):
+    return self.name
+
 class ServerType(CommonInfo):
   pass
 
-class Diskype(CommonInfo):
+class DiskType(CommonInfo):
   pass
 
 class Owner(CommonInfo):
@@ -16,4 +19,30 @@ class Owner(CommonInfo):
 
 class Location(CommonInfo):
   pass
+
+class Server(models.Model):
+
+  options = (
+    ('gb', 'GB'),
+    ('tb', 'TB'),
+  )
+
+  name = models.CharField(max_length=63)
+  
+  cpu = models.CharField(max_length=63)
+  ram = models.CharField(max_length=3, choices=options, default='gb')
+  ip_address = models.CharField(max_length=63)
+  username = models.CharField(max_length=63)
+  password = models.CharField(max_length=63)
+  public_ip = models.CharField(max_length=63)
+  private_ip = models.CharField(max_length=63)
+  remarks = models.TextField(blank=True, null=True)
+  status = models.BooleanField(default=False)
+  server_location = models.ForeignKey(Location, on_delete=models.CASCADE)
+  server_type = models.ForeignKey(ServerType, on_delete=models.CASCADE)
+  disk_type = models.ForeignKey(DiskType, on_delete=models.CASCADE)
+  Owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.name
 
